52e9b244c20c56bd66c17645dd1cc7fb0117edb1
Bannawat <109212993+Bannawat01@users.noreply.github.com>
2025-07-14 21:56:37 +0700

    Add middleware for CORS, body limit, and timeout; implement panic endpoint

--- go.mod
+++ go.mod
@@ -19,6 +19,7 @@ require (
 	github.com/go-playground/locales v0.14.1 // indirect
 	github.com/go-playground/universal-translator v0.18.1 // indirect
 	github.com/go-viper/mapstructure/v2 v2.2.1 // indirect
+	github.com/golang-jwt/jwt v3.2.2+incompatible // indirect
 	github.com/jackc/pgpassfile v1.0.0 // indirect
 	github.com/jackc/pgservicefile v0.0.0-20240606120523-5a60cdf6a761 // indirect
 	github.com/jackc/pgx/v5 v5.6.0 // indirect
@@ -44,5 +45,6 @@ require (
 	golang.org/x/sync v0.14.0 // indirect
 	golang.org/x/sys v0.33.0 // indirect
 	golang.org/x/text v0.25.0 // indirect
+	golang.org/x/time v0.11.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- server/server.go
+++ server/server.go
@@ -8,9 +8,11 @@ import (
 	"os/signal"
 	"sync"
 	"syscall"
+	"time"
 
 	"github.com/Bannawat101/project-shop-api/config"
 	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 	"github.com/labstack/gommon/log"
 	"gorm.io/gorm"
 )
@@ -42,8 +44,20 @@ func NewEchoServer(conf *config.Config, db *gorm.DB) *echoServer {
 }
 
 func (s *echoServer) Start() {
+	corsMiddleware := getCORSMMiddleware(s.conf.Server.AllowOrigins)
+	bodyLimitMiddleware := getBoddyLimitMiddleware(s.conf.Server.BodyLimit)
+	timeOutMiddleware := getTimeoutMiddleware(s.conf.Server.Timeout)
+
+	s.app.Use(middleware.Recover())
+	s.app.Use(middleware.Logger())
+	s.app.Use(corsMiddleware)
+	s.app.Use(bodyLimitMiddleware)
+	s.app.Use(timeOutMiddleware)
 
 	s.app.GET("/v1/health", s.healthCheck)
+	s.app.GET("/v1/panic", func(c echo.Context) error {
+		panic("Panic")
+	})
 
 	quitCh := make(chan os.Signal, 1)
 
@@ -74,4 +88,29 @@ func (s *echoServer) gracefulShutdown(quitCh chan os.Signal) {
 
 func (s *echoServer) healthCheck(c echo.Context) error {
 	return c.String(http.StatusOK, "OK")
+}
+
+func getLoggerMiddleware() echo.MiddlewareFunc {
+	return middleware.Logger()
+}
+
+func getTimeoutMiddleware(timeOut time.Duration) echo.MiddlewareFunc { //กำหนดเวลา timeout ของ request
+	return middleware.TimeoutWithConfig(middleware.TimeoutConfig{
+		Skipper:      middleware.DefaultSkipper,
+		ErrorMessage: "Request timeout",
+		Timeout:      timeOut * time.Second,
+	})
+}
+
+func getCORSMMiddleware(allawOrigin []string) echo.MiddlewareFunc {
+	return middleware.CORSWithConfig(middleware.CORSConfig{
+		Skipper:      middleware.DefaultSkipper,
+		AllowOrigins: allawOrigin,
+		AllowMethods: []string{echo.GET, echo.POST, echo.PUT, echo.DELETE, echo.PATCH},       //กำหนด method ที่อนุญาตให้ใช้ได้
+		AllowHeaders: []string{echo.HeaderOrigin, echo.HeaderContentType, echo.HeaderAccept}, //กำหนด header ที่อนุญาตให้ใช้ได้
+	})
+}
+
+func getBoddyLimitMiddleware(bodyLimt string) echo.MiddlewareFunc {
+	return middleware.BodyLimit(bodyLimt)
 }
